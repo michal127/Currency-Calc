1f9e8b6663f03a1e488da3677ab19297cdfccdd6
Michał <mic127@onet.eu>
2018-07-09 13:53:48 +0200

    Dodano klasę służącą do pobierania wartości każdej waluty, usunięto klasę "functions".

---
+++ Walutomat/CurrencyValue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace walutomat
+{
+    class CurrencyValue                                          
+    {
+        private Double value;
+        public Double getValue()
+        {
+            return value;
+        }
+        public void downloadValue(String link, String pattern)     //Method for filltering html code for currency value using regex function
+        {
+            String html_code = getWeb(link);                       //Downloading entire html code in simple string using getWeb function
+            value = Convert.ToDouble(Regex.Match(html_code, pattern).Groups[1].Value);   //Filtering html code using regex pattern
+        }
+        private String getWeb(String link)                         //Method for downloading html code from given link
+        {
+            WebClient web = new WebClient();
+            String html = web.DownloadString(link);
+            return html;
+        }
+
+    }
+}
--- Walutomat/Okno_główne.cs
+++ Walutomat/Okno_główne.cs
@@ -16,7 +16,7 @@ namespace walutomat
 {
     public partial class Okno_główne : Form
     {
-        Functions functions = new Functions();
+      
 
         public Double eurValue;
         public Double gbpValue;
@@ -38,9 +38,9 @@ namespace walutomat
         
         public void Clear()                       //Method for clearing following currencies 
         {
-            functions.ClearSingleCurrency(eurTextBox, eurResult, eurERR);
-            functions.ClearSingleCurrency(gbpTextBox, gbpResult, gbpERR);
-            functions.ClearSingleCurrency(usdTextBox, usdResult, usdERR);
+            ClearSingleCurrency(eurTextBox, eurResult, eurERR);
+            ClearSingleCurrency(gbpTextBox, gbpResult, gbpERR);
+            ClearSingleCurrency(usdTextBox, usdResult, usdERR);
         }
         
 
@@ -48,40 +48,77 @@ namespace walutomat
 
         public void getValues()  //Method for downloading currency values from bankier.pl
         {
-                               
-            eurValue = functions.getValue("https://www.bankier.pl/waluty/kursy-walut/forex/EURPLN", @"profilLast\S{2}(.*)\s\S{2}div");                                           //Function for filtering html code using our regex pattern
-            gbpValue = functions.getValue("https://www.bankier.pl/waluty/kursy-walut/forex/GBPPLN", @"profilLast\S{2}(.*)\s\S{2}div");
-            usdValue = functions.getValue("https://www.bankier.pl/waluty/kursy-walut/forex/USDPLN", @"profilLast\S{2}(.*)\s\S{2}div");
+            CurrencyValue EUR = new CurrencyValue();
+            CurrencyValue GBP = new CurrencyValue();
+            CurrencyValue USD = new CurrencyValue();
+            EUR.downloadValue("https://www.bankier.pl/waluty/kursy-walut/forex/EURPLN", @"profilLast\S{2}(.*)\s\S{2}div");
+            GBP.downloadValue("https://www.bankier.pl/waluty/kursy-walut/forex/GBPPLN", @"profilLast\S{2}(.*)\s\S{2}div");
+            USD.downloadValue("https://www.bankier.pl/waluty/kursy-walut/forex/USDPLN", @"profilLast\S{2}(.*)\s\S{2}div");
+            eurValue = EUR.getValue();                                        
+            gbpValue = GBP.getValue();
+            usdValue = USD.getValue();
             eurCurrency.Text = eurValue + "zł";           
             gbpCurrency.Text = gbpValue + "zł";
             usdCurrency.Text = usdValue + "zł";
         }
 
-    
+        public void CountCurrency(Label ERRlabel, TextBox CurrResult, TextBox CurrTextBox, Double CurrValue, bool Reverse) //Method for calculating currency value and setting up labels
+        {
+
+            try
+            {
+                ERRlabel.Text = "";
+                double currencyResult;
+                if (!Reverse)  //checking direction of
+                {
+                    currencyResult = Math.Round(CurrValue * double.Parse(CurrTextBox.Text.Replace('.', ',')), 2);
+                    CurrResult.Text = currencyResult.ToString();
+                }
+                else
+                {
+                    currencyResult = Math.Round(double.Parse(CurrResult.Text.Replace('.', ',')) / CurrValue, 2);
+                    CurrTextBox.Text = currencyResult.ToString();
+                }
+            }
+            catch (FormatException fEx)
+            {
+                ERRlabel.Text = "Type in correct value";
+                CurrTextBox.Text = "";
+                CurrResult.Text = "";
+            }
+        }
+
+
+        public void ClearSingleCurrency(TextBox Text, TextBox ResultText, Label ErrText) //Method for clearing all textboxes and labels for single currency
+        {
+            Text.Text = "";
+            ResultText.Text = "";
+            ErrText.Text = "";
+        }
 
         private void buttonEUR_Click(object sender, EventArgs e)
         {
-            functions.CountCurrency(eurERR, eurResult, eurTextBox, eurValue, false);
+            CountCurrency(eurERR, eurResult, eurTextBox, eurValue, false);
         }
         private void buttonGBP_Click(object sender, EventArgs e)
         {
-            functions.CountCurrency(gbpERR, gbpResult, gbpTextBox, gbpValue, false);
+            CountCurrency(gbpERR, gbpResult, gbpTextBox, gbpValue, false);
         }
         private void buttonUSD_Click(object sender, EventArgs e)
         {
-            functions.CountCurrency(usdERR, usdResult, usdTextBox, usdValue, false);
+            CountCurrency(usdERR, usdResult, usdTextBox, usdValue, false);
         }
         private void buttonEURrev_Click(object sender, EventArgs e)
         {
-            functions.CountCurrency(eurERR, eurResult, eurTextBox, eurValue, true);
+            CountCurrency(eurERR, eurResult, eurTextBox, eurValue, true);
         }
         private void buttonGBPrev_Click(object sender, EventArgs e)
         {
-            functions.CountCurrency(gbpERR, gbpResult, gbpTextBox, gbpValue, true);
+            CountCurrency(gbpERR, gbpResult, gbpTextBox, gbpValue, true);
         }
         private void buttonUSDrev_Click(object sender, EventArgs e)
         {
-            functions.CountCurrency(usdERR, usdResult, usdTextBox, usdValue, true);
+            CountCurrency(usdERR, usdResult, usdTextBox, usdValue, true);
         }
         private void ClearAll_Click(object sender, EventArgs e)
         {
@@ -91,7 +128,7 @@ namespace walutomat
         {
             Close();
         }
-        private void timer_Tick(object sender, EventArgs e)
+        private void timer_Tick(object sender, EventArgs e) //Auto refreshing currency values every 60 seconds
         {
             getValues();
         }
